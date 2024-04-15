6e7f6f02739377c3b8f5342e029fd7e366355895
Lamark <lamark12ricarte@gmail.com>
2024-04-15 11:01:14 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,32 @@
+package com.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transaction")
+@Table(name = "transaction")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
